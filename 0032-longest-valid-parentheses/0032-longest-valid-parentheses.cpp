4601d977a9b77225a502c4adcf33e92c7271d1bc
Charlie G. <cgarest21@gmail.com>
2025-11-15 22:55:26 -0600

    Time: 0 ms (100%), Space: 11.6 MB (82.19%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int max_length =0;
+
+        stack<int> stack;
+
+        stack.push(-1);
+
+        for (int i = 0; i < s.size(); i++){
+            
+            if (s[i] == '('){
+                stack.push(i);
+            }
+
+            else{
+                stack.pop();
+                if(stack.empty()){
+                    stack.push(i);
+                }
+                else{
+                    max_length = max(max_length,i - stack.top());
+                }
+            }
+        }
+
+        return max_length;
+    }
+};
